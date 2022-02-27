0b37e8e38801755c650d72c84898b0dfc0cc0908
Michaël Boeyens <michael.boeyens@gmail.com>
2022-02-27 21:54:41 +0100

    moved keyboard to seperate component

---
+++ upgrade/src/components/CalcKeyBoard.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="keyboard">
+    <div
+      v-for="(item, index) in buttonGrid"
+      :key="'gridrow ' + (index + 1)"
+      class="keyboard__row"
+    >
+      <CustomButton v-for="subitem in item" :key="subitem">
+        {{ subitem }}
+      </CustomButton>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import CustomButton from "./CustomButton.vue";
+
+const buttonGrid = [
+  ["%", "CE", "C", "DEL"],
+  ["1/x", "x²", "√", "÷"],
+  ["7", "8", "9", "x"],
+  ["4", "5", "6", "-"],
+  ["1", "2", "3", "+"],
+  ["±", "0", ",", "="],
+];
+</script>
+
+<style lang="scss" scoped>
+.keyboard {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(6, 1fr);
+  gap: 3px;
+  width: 100%;
+  &__row {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: 1fr;
+    gap: 3px;
+    width: 100%;
+  }
+}
+</style>
--- upgrade/src/views/HomeView.vue
+++ upgrade/src/views/HomeView.vue
@@ -1,30 +1,15 @@
 <template>
   <main class="calc">
     <CalcScreen class="calc__screen" />
-    <div class="calc__container">
-      <div v-for="(item, index) in buttonGrid" :key="'gridrow ' + (index + 1)">
-        <CustomButton v-for="subitem in item" :key="subitem">
-          {{ subitem }}
-        </CustomButton>
-      </div>
-    </div>
+    <CalcKeyBoard class="calc__keyboard" />
     <CustomAside class="calc__history" />
   </main>
 </template>
 
 <script setup lang="ts">
 import CalcScreen from "@/components/CalcScreen.vue";
-import CustomButton from "@/components/CustomButton.vue";
 import CustomAside from "@/components/CustomAside.vue";
-
-const buttonGrid = [
-  ["%", "CE", "C", "DEL"],
-  ["1/x", "x²", "√", "÷"],
-  ["7", "8", "9", "x"],
-  ["4", "5", "6", "-"],
-  ["1", "2", "3", "+"],
-  ["±", "0", ",", "="],
-];
+import CalcKeyBoard from "@/components/CalcKeyBoard.vue";
 </script>
 
 <style lang="scss" scoped>
@@ -34,24 +19,19 @@ const buttonGrid = [
   margin: 0 auto;
   display: grid;
   grid-template-columns: 5fr 4fr;
-  grid-template-rows: 4fr 5fr 5fr;
-
+  grid-template-rows: 4fr 10fr;
   &__screen {
     grid-row: 1/2;
     grid-column: 1/2;
   }
-  &__container {
-    grid-row: 2/4;
+  &__keyboard {
+    grid-row: 2/3;
     grid-column: 1/2;
     background-color: lightgray;
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-template-rows: repeat(6, 1fr);
-    gap: 3px;
-    padding: 3px;
+    width: 100%;
   }
   &__history {
-    grid-row: 1/4;
+    grid-row: 1/3;
     grid-column: 2/3;
   }
 }
